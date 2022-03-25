0ea41c6512c704bd85e71aea18d4f3e07c6b8494
Henrique Kubinhetz <rickubinhetz@gmail.com>
2022-03-25 00:06:06 -0300

    Simple flask exercise to get a hold of request structures. This knowledge will be used on my blog that I'm building as a course project.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.route("/login", methods=["POST"])
+def post_form():
+    form_info = request.form.to_dict()
+    print(f"Data: {form_info}")
+    return render_template('login.html', data=form_info)
 
 
 if __name__ == "__main__":
